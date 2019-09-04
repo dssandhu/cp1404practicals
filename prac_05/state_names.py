436b074642b238f3520b6e573063c7b20b04621b
dssandhu <damandeep.sandhu@my.jcu.edu.au>
2019-09-04 14:39:25 +1000

    add loop

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -16,3 +16,6 @@ while state != "":
     else:
         print("Invalid short state")
     state = input("Enter short state: ").upper()
+
+for state in STATE_NAMES:
+    print(state, "is", STATE_NAMES[state])
