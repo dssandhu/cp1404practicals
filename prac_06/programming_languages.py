4374ac72277922ab5542104105ec33141a43e928
dssandhu <damandeep.sandhu@my.jcu.edu.au>
2019-09-11 14:33:08 +1000

    fixed indent

--- prac_06/programming_languages.py
+++ prac_06/programming_languages.py
@@ -7,8 +7,7 @@ class ProgrammingLanguage:
         self.year = year
 
     def __str__(self):
-        return "{}, {} Typing, Reflection={}, First appeared in {}".format(
-            self.name, self.typing, self.reflection, self.year)
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection, self.year)
 
     def is_dynamic(self):
         return self.typing == "Dynamic"
