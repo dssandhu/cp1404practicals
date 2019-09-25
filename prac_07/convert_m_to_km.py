bdd66977827586c9124d890a5c91861a17343538
dssandhu <damandeep.sandhu@my.jcu.edu.au>
2019-09-25 14:42:56 +1000

    fixed error

--- prac_07/convert_m_to_km.py
+++ prac_07/convert_m_to_km.py
@@ -1,8 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
 
-MILES_TO_KM = 1.60934
-
 
 class MilesConverterApp(App):
     def build(self):
@@ -12,7 +10,7 @@ class MilesConverterApp(App):
 
     def handle_calculate(self):
         value = self.get_validated_miles()
-        result = value * MILES_TO_KM
+        result = value * 1.60934
         self.root.ids.output_label.text = str(result)
 
     def handle_increment(self, change):
